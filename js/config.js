a82e55df3b8388b6ad8025b404249d5c5ab73e66
Filippo Rossi <rf.rossi.filippo@gmail.com>
2026-09-02 14:06:16 +0200

    Configura repository

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  owner: "potesplitnew",
+  repo: "prova",
+  branch: "main",
+  homeUrl: "https://potewww.github.io/potesplitnew/",
+  homeOwner: "potewww",
+  homeRepo: "potesplitnew",
+  homeBranch: "main"
+};
